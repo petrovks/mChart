af353815e72b9eecf2d6952c5af46baab47b5b3a
petrovks <38166811+petrovks@users.noreply.github.com>
2021-03-10 15:45:47 +0300

    Update Controller.java

--- client/src/main/java/geekbrains/mchart/Controller.java
+++ client/src/main/java/geekbrains/mchart/Controller.java
@@ -6,8 +6,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
